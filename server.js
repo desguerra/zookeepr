fba49033e99e4cdb31fb62e7c87bdb66330ae7ad
desguerra <ddesguerra@ucdavis.edu>
2022-09-09 13:48:52 -0700

    connect front end to functionality

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+// instructs the server to make certain files readily available
+// and to not gate it behind a server endpoint (for CSS and JS files)
+// AKA make these files static resources, so that all of our front-end
+// code can be accessed w/o having a specific server endpt. created for it
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -123,6 +129,25 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+// set up html files
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+// if user names a request to routes that dont exist (wildcard routes)
+// ORDER MATTERS! windcard route should be after all the other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // make our server listen with listen() method
